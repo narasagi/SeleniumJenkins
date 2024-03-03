567628b1a450289fc74821d4cfac9dcf27332819
narasagi <narasimhulu.sagi@gmail.com>
2024-03-03 12:11:08 -0500

    updated login page related tests

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,37 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+
+    private WebDriver driver;
+    private By emailLocator = By.name("email");
+    private By pwd = By.name("passwor");
+    private By loginBtn = By.xpath("//input[@type='submit']");
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+    public void enterEmail(String email){
+        WebElement emailAddress =  driver.findElement(emailLocator);
+        emailAddress.sendKeys(email);
+    }
+
+    public void enterPassword(String password){
+        WebElement pwdInput = driver.findElement(pwd);
+        pwdInput.sendKeys(password);
+    }
+
+    public void clickLoginBtn(){
+        WebElement loginButton = driver.findElement(loginBtn);
+        loginButton.click();
+    }
+
+  /* public void login(String email, String password){
+       enterEmail(email);
+       enterPassword(password);
+       clickLoginBtn();
+   }*/
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,32 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
 public class LoginTest {
+    private WebDriver driver;
+   public LoginPage loginPage;
+    @BeforeTest
+    public void setUp(){
+        driver = new FirefoxDriver();
+        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+    }
+    @Test
+    public void loginTest() throws InterruptedException {
+        loginPage = new LoginPage(driver);
+        loginPage.enterEmail("qatestertest@gmail.com");
+        loginPage.enterPassword("Test@123");
+        loginPage.clickLoginBtn();
+        Thread.sleep(5);
+    }
+    @AfterMethod
+    public void tearDown(){
+        if (driver != null){
+            driver.quit();
+        }
+    }
 }
